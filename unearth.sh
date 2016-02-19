37b74bd487497003885faca1f66d37aed46e51e4
MoeBrowne <oliver.browne@uplyme.com>
2016-02-19 22:19:50 +0000

    Looped over a number of grouped DNS servers and output the A records they have

--- unearth.sh
+++ unearth.sh
@@ -2,7 +2,23 @@
 
 domainName=$1
 
-DIGOUTPUT=$(dig +nocmd +noall +answer "$domainName" A)
+declare -A NAMESERVERS
+
+NAMESERVERS[Google]="8.8.8.8 8.8.4.4"
+NAMESERVERS[OpenDNS]="208.67.222.222 208.67.220.220 208.67.222.220 208.67.220.222"
+NAMESERVERS[OpenDNSFamilyShield]="208.67.222.123 208.67.220.123"
+
+for NSGroupName in "${!NAMESERVERS[@]}"; do
+
+    NSGroupIPs=(${NAMESERVERS[$NSGroupName]})
+
+    for NSGroupIP in "${NSGroupIPs[@]}"; do
+        DIGOUTPUT=$(echo -n "$NSGroupName $NSGroupIP " && dig +nocmd +noall +answer "$domainName" A @$NSGroupIP)
+        echo "$DIGOUTPUT"
+    done
+
+done
+exit;
 
 # Loop through each of the records returned from dig
 while read -r record; do
