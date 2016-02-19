90314e17bde57e11d544bb254076e24a67f63012
MoeBrowne <oliver.browne@uplyme.com>
2016-02-19 22:27:10 +0000

    Added a column header and tab separated the NS Group Name and NS IP

--- unearth.sh
+++ unearth.sh
@@ -8,12 +8,14 @@ NAMESERVERS[Google]="8.8.8.8 8.8.4.4"
 NAMESERVERS[OpenDNS]="208.67.222.222 208.67.220.220 208.67.222.220 208.67.220.222"
 NAMESERVERS[OpenDNSFamilyShield]="208.67.222.123 208.67.220.123"
 
+echo "NS Group Name	NS IP	Domain	TTL	IN	Record Type	IP"
+
 for NSGroupName in "${!NAMESERVERS[@]}"; do
 
 	NSGroupIPs=(${NAMESERVERS[$NSGroupName]})
 
 	for NSGroupIP in "${NSGroupIPs[@]}"; do
-		DIGOUTPUT=$(echo -n "$NSGroupName $NSGroupIP " && dig +nocmd +noall +answer "$domainName" A @$NSGroupIP)
+		DIGOUTPUT=$(echo -n "$NSGroupName	$NSGroupIP	" && dig +nocmd +noall +answer "$domainName" A @$NSGroupIP)
 		echo "$DIGOUTPUT"
 	done
 
