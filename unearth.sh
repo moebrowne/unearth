babaab406ef88b42d2903967d160311a96a72177
MoeBrowne <oliver.browne@uplyme.com>
2016-04-24 00:44:39 +0100

    Used a regex match to get just the IP reported by the NS

--- unearth.sh
+++ unearth.sh
@@ -13,15 +13,19 @@ NAMESERVERS[DNSWatch]="84.200.69.80 84.200.70.40"
 NAMESERVERS[DYN]="216.146.35.35 216.146.36.36"
 NAMESERVERS[Norton]="199.85.126.10 199.85.127.10"
 
-echo "NS Group Name	NS IP	Domain	TTL	IN	Record Type	IP"
+echo "Name Server	Reported IP"
 
 for NSGroupName in "${!NAMESERVERS[@]}"; do
 
 	NSGroupIPs=(${NAMESERVERS[$NSGroupName]})
 
 	for NSGroupIP in "${NSGroupIPs[@]}"; do
-		DIGOUTPUT=$(echo -n "$NSGroupName	$NSGroupIP	" && dig +nocmd +noall +answer "$domainName" A @$NSGroupIP)
-		echo "$DIGOUTPUT"
+		DIGOUTPUT=$(dig +nocmd +noall +answer "$domainName" A @$NSGroupIP)
+
+		regexDNSRecord="^([[:alnum:]\-\.]+)[[:space:]]+([0-9]+)[[:space:]]+(IN)[[:space:]]+([[:alnum:]]+)[[:space:]]+(.+)$"
+		[[ $DIGOUTPUT =~ $regexDNSRecord ]]
+
+		echo "$NSGroupName		${BASH_REMATCH[5]}"
 	done
 
 done
